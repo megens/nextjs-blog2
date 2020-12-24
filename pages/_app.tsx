ce5e871d4c6806017581a68f137235ec92749578
megens <robert.megens@gmail.com>
2020-12-24 00:16:34 -0500

    adding some material UI functionality to check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,29 @@
 import "../styles/global.css";
 import { AppProps } from "next/app";
+import { useEffect } from 'react';
+
+/*
+according to 
+https://dev.to/felixmohr/setting-up-a-blog-with-next-js-react-material-ui-and-typescript-2m6k
+
+2. In your _app.tsx, apply a useEffect hook to remove the CSS that were injected on server-side from the client-side app:
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+By doing this, we allow the client to take over styling the app as soon as its ready.
+
+*/
+
+useEffect(() => {
+  const jssStyles = document.querySelector('#jss-server-side')
+  if (jssStyles) {
+    jssStyles.parentElement.removeChild(jssStyles)
+  }
+}, [])
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />; 
