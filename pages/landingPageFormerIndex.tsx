a2ca75180ea3f9aa024bd3b95cc093345177d912
megens <robert.megens@gmail.com>
2020-12-25 14:55:38 -0500

    first material ui Header component

---
+++ pages/landingPageFormerIndex.tsx
@@ -0,0 +1,147 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import DateComponent from "../components/date"; 
+import { GetStaticProps } from "next";
+
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Toolbar from '@material-ui/core/Toolbar'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Hidden from '@material-ui/core/Hidden'
+//import Link from '@material-ui/core/Link'
+// RESOLVE CONFLICT WITH NEXT LINK
+// https://dev.to/ivandotv/using-next-js-link-component-with-material-ui-buttons-and-menu-items-3m6a
+import Container from '@material-ui/core/Container'
+
+// this doesn't match anything ... mixing examples here
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`
+  },
+  toolbarTitle: {
+    flex: 1
+  },
+  mainFeaturedPost: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.7)'
+  },
+  mainFeaturedPostContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0
+    }
+  },
+  card: {
+    display: 'flex'
+  },
+  cardDetails: {
+    flex: 1
+  },
+  cardMedia: {
+    width: 160
+  }
+}))
+
+
+
+
+export default function Home(
+  {buildTimeStamp, allPostsData}
+  // RfM NOTE that I haven't "typed" these correctly. Not sure how to do that.
+  // the prop "home" used in Layout seems to be an intrinsic value, based on "is this the Home page?"
+
+  /*
+  {
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}
+*/
+) {
+  return (
+    <Layout home> 
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello. I'm Rob. I'm an actuary, coder, blockchain developer and
+          sailor.
+        </p>
+        <p>
+          (Build Time: {buildTimeStamp} UTC @ production)
+        </p>
+        
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <DateComponent dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+        
+      </section>
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  //let unix_timestamp = 1549312452
+  let timestamp:number = Date.now();
+  let buildDate:Date = new Date(timestamp);
+  let buildHours:string = buildDate.getHours().toString();
+  let buildMinutes:string = "0" + buildDate.getMinutes();
+  let buildSeconds:string = "0" + buildDate.getSeconds();
+  const buildTimeStamp:string = buildHours+":"+buildMinutes.substr(-2)+":"+buildSeconds.substr(-2)
+  const allPostsData = getSortedPostsData(); 
+  return {
+    props: {
+      allPostsData,
+      buildTimeStamp
+    },
+  };
+};
