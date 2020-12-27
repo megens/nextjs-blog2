b4c7fa9a433d96d5839d2dea87d9c5f71529bc9b
megens <robert.megens@gmail.com>
2020-12-27 10:02:27 -0500

    try again

--- pages/landingPageFormerIndex.js
+++ pages/landingPageFormerIndex.js
@@ -1,3 +1,4 @@
+/*
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
@@ -87,6 +88,8 @@ export default function Home(
   }[];
 }
 */
+
+/*
 ) {
   return (
     <Layout home> 
@@ -145,3 +148,4 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
+*/
