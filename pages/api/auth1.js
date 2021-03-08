352e3811ea55be2f61ef44b9121e1f8eac834354
Ro <robert.megens@gmail.com>
2021-03-08 16:58:19 -0500

    try auth1 again

--- pages/api/auth1.js
+++ pages/api/auth1.js
@@ -27,6 +27,31 @@ function authUser(db, email, password, hash, callback) {
 */
 
 // TO DO ... type this req.body better!
+
+import { connectToDatabase } from "../../util/mongodb";
+
+export default async (req, res) => {
+  /*  const { db } = await connectToDatabase();
+
+  const movies = await db
+    .collection("movies")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  res.json(movies);
+  */
+
+  console.log("about to dbConnect");
+  await dbConnect();
+  console.log("readyState");
+  console.log(mongoose.connection.readyState);
+
+  res.json({ result: true });
+};
+
+/*
 function validate(req) {
   console.log("validating sign in attempt ...");
   const joiSchema = Joi.object({
@@ -83,3 +108,5 @@ export default async (req, res) => {
     return;
   }
 };
+
+*/
